20a1723403f0a389abc2f2ba5e82b80324e211f3
RiceTheDev <151386744+RiceTheDev@users.noreply.github.com>
2025-10-19 21:25:17 -0300

    feat: add assets/templates

---
+++ assets/blog.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('/api/posts')
+        .then(response => response.json())
+        .then(data => {
+            const container = document.getElementById("posts")
+            
+            data.forEach(post => {
+                const postElement = `<article class="post">
+                    <h2>${post.title}</h2>
+                    ${post.content.length < 100 ? `<p>${post.content}</p>` : `<p>${post.content.substring(0, 100)}...</p>`}
+                    ${post.image ? `<img src="${post.image}" class="post-image" alt="Post Image"/>` : ''}
+                    <div class="sep"></div>
+                </article>`
+                container.innerHTML += postElement
+            })
+        })
+        //.catch(error => console.error('Error fetching posts:', error))
+});
